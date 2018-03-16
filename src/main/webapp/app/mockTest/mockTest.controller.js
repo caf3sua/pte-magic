ad50ba7c4d728f9553a2bf365d8809273a0e0752
Phuongit247 <phuongit247@gmail.com>
2018-03-16 14:36:05 +0700

    fix ui

--- src/main/webapp/app/mockTest/mockTest.controller.js
+++ src/main/webapp/app/mockTest/mockTest.controller.js
@@ -9,5 +9,14 @@
 
     function MockTestController ($scope, Principal, LoginService, $state, $rootScope, $timeout) {
         var vm = this;
+        vm.pastA = pastA;
+        vm.listQuestionShow = false;
+        function pastA() {
+            $timeout(function (){
+                angular.element('#pastA').addClass("active");
+                angular.element('#pastAMobile').addClass("activeMobile");
+            });
+            vm.listQuestionShow = true;
+        }
     }
 })();
