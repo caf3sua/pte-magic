1630a7828315b8d5201f64a34eb5f6ec635f1523
Phuongna <phuongna@itsol.vn>
2018-04-04 14:27:24 +0700

    add class

--- src/main/webapp/app/fulltest/fulltest.controller.js
+++ src/main/webapp/app/fulltest/fulltest.controller.js
@@ -10,13 +10,13 @@
 
     function FullTestController ($controller, $scope, $window, $stateParams, Principal, LoginService, $state
     		, $rootScope, $timeout, ExamType, Exam, Answer, Upload, $sce) {
-        
+
     	var vm = this;
     	// Function
     	vm.answer = answer;
     	vm.closeExam = closeExam;
     	vm.trustAsHtml = $sce.trustAsHtml;
-    	
+
     	// Variable, flag
     	vm.examTypeId;
     	vm.exam;
@@ -33,19 +33,19 @@
     	vm.toggleRecording = toggleRecording;
     	vm.startRecording = startRecording;
     	vm.startNewPart = startNewPart;
-    	
+
     	vm.txtInfoCountdown = "Begining in ";
     	vm.countdownRecording = 5;
     	vm.isRecording = false;
-    	
+
     	vm.countdownTimebreak = 600;
-    	
+
     	vm.btnTxt = 'Answer';
-    	
+
     	function startNewPart() {
     		nextQuestion();
     	}
-    	
+
     	function startRecording() {
     		// start recording
 	        if (!audioRecorder)
@@ -56,14 +56,14 @@
 	        vm.txtInfoCountdown = "Recording ..."
         	vm.isRecording = true;
     	}
-    	
+
     	function stopRecording() {
     		// stop recording
 	        audioRecorder.stop();
 	        audioRecorder.getBuffers( gotBuffers );
 	        vm.btnEnable = true;
     	}
-    	
+
 //    	function toggleRecording() {
 //    	    if ($('#record').hasClass("recording")) {
 //    	        // stop recording
@@ -80,58 +80,58 @@
 //    	        audioRecorder.record();
 //    	    }
 //    	}
-    	
+
     	function initPlayer() {
     		var audio = $("#player");
     		if (audio[0] != undefined) {
     			audio[0].addEventListener('ended', callBackAudioEnded);
     		}
     	}
-    	
+
     	function callBackAudioEnded() {
     		console.log('audio ended!');
     	}
-    	
+
     	function playAudio(link, timeout) {
-    		var audio = $("#player");  
+    		var audio = $("#player");
     		if (audio[0] != undefined) {
     			$("#mp3_src").attr("src", link); // https://storage.googleapis.com/pte-magic/CHINA_1.mp3
                 audio[0].pause();
                 audio[0].load();
-                
+
                 $timeout(function(){
                 	audio[0].play();
                 }, timeout );
     		}
-            
+
     	}
-    	
+
     	function initAnswer() {
     		// Stop audio
-    		var audio = $("#player");     
+    		var audio = $("#player");
     		if (audio[0] != undefined) {
     			audio[0].pause();
     		}
-            
-            
+
+
             // Stop timer
             $scope.$broadcast('timer-stop');
   			$scope.$broadcast('timer-reset');
-  			
+
   			vm.txtInfoCountdown = "Begining in ";
   	    	vm.isRecording = false;
     	}
-    	
+
     	angular.element(document).ready(function () {
     		$timeout(function(){
 	    		// Load player
 	    		initPlayer();
-	    		
+
 	    		// Load record audio
 	    		initAudio();
     		}, 1000 );
         });
-    	
+
     	function initMockTest() {
     		if (vm.exam.examTypeDTO.type == 'MOCK_TEST_A') {
     			vm.countdown = 110 * 60;
@@ -141,19 +141,19 @@
     			vm.countdown = 200 * 60;
     		}
     	}
-    	
+
     	// Init controller
   		(function initController() {
   			// Calculate countdown
-  			// PartA: 110, PartB: 90, Full: 200 
-  			
+  			// PartA: 110, PartB: 90, Full: 200
+
   			// instantiate base controller
 			//$controller('PteMagicBaseController', {
 			//	vm : vm
 			//});
-  					
+
   			vm.examTypeId = $stateParams.type;
-  			
+
   			Exam.startExams({
   				examTypeId: vm.examTypeId
             }, onSuccess, onError);
@@ -161,10 +161,10 @@
             	vm.exam = data;
             	vm.questions = data.questions;
             	console.log(data);
-            	
+
             	// Init mocktest
             	initMockTest();
-            	
+
             	// Next question
             	nextQuestion();
             }
@@ -175,10 +175,10 @@
   		function closeExam() {
   			$window.close();
   		}
-  		
+
   		function getUserAnswer() {
   			vm.answers = [];
-  			
+
   			if (vm.selectedQuestion.type == 'LISTENING_FIB_L') {
   				$('.input_answer').each(function(){
   					vm.answers.push($(this).val());
@@ -192,10 +192,10 @@
   	            });
   			}
   		}
-  		
+
   		function answer() {
   			initAnswer();
-  			
+
   			// Upload if questionGroup == SPEAKING
   			if (vm.questionGroup == 'SPEAKING') {
   				stopRecording();
@@ -205,15 +205,15 @@
   	  			// Get answer
   	  			getUserAnswer();
   	  			console.log(vm.answers);
-  	  			
+
   	  			// Save answer
   	  			saveAnswer();
   			}
-  			
+
   			// Next question
   			nextQuestion();
   		}
-  		
+
   		$scope.$watch('vm.fileUpload', function (file) {
   			var file;
   			if (file) {
@@ -231,12 +231,12 @@
                   if (response.status > 0)
                       $scope.errorMsg = response.status + ': ' + response.data;
               }, function (evt) {
-                  file.progress = Math.min(100, parseInt(100.0 * 
+                  file.progress = Math.min(100, parseInt(100.0 *
                                            evt.loaded / evt.total));
               });
-          }  
+          }
         });
-  		
+
   		function uploadRecording(selectedQuestionId) {
   			var blobUrl = $("#save").attr('href');
   			console.log(blobUrl);
@@ -250,23 +250,23 @@
   			    	// myBlob is now the blob that the object URL pointed to.
   			    	var filename = "recording_" + vm.exam.examDTO.id + "_" + selectedQuestionId + ".wav";
   			    	vm.fileUpload = new File([blob], filename);
-  			    	
+
   			    	// save answer
   			    	saveAnswerSpeaking(selectedQuestionId, filename);
   			  }
   			};
   			xhr.send();
   		}
-  		
+
   		function updateQuestionInfo(selQuestion) {
   			// Replace @Blank@
   			if (selQuestion.type == 'LISTENING_FIB_L') {
-  				selQuestion.description = selQuestion.description.replace(/@Blank@/g, '<input type="text" name="input" class="input_answer"/>');
+  				selQuestion.description = selQuestion.description.replace(/@Blank@/g, '<input type="text" name="input" class="input_answer pte-writing-input"/>');
   				//selQuestion.description.split('@Blank@').join('xxxxxxx');
   			}
   		}
-  		
-  		
+
+
   		function nextQuestion() {
   			vm.selectedQuestion = vm.questions.shift();
   			if (vm.selectedQuestion == null || vm.selectedQuestion == undefined) {
@@ -292,33 +292,33 @@
   				} else {
   					vm.btnTxt = 'Answer';
   				}
-  				
+
   				// Get question group
   				updateQuestionInfo(vm.selectedQuestion);
-  				
+
   				console.log(vm.selectedQuestion);
-  				
+
   				vm.questionGroup = getQuestionGroup(vm.selectedQuestion.type);
   				console.log(vm.questionGroup);
-  				
+
   				// Enable/disable button Answer
   				if (vm.questionGroup == 'SPEAKING') {
   					vm.btnEnable = false;
   				} else {
   					vm.btnEnable = true;
   				}
-  				
+
   				$scope.$broadcast('timer-start');
   				playAudio(vm.selectedQuestion.audioLink, 3000);
   			}
   		}
-  		
+
   		function saveAnswer() {
   			// Skip if time break
   			if (vm.selectedQuestion.type == 'TIME_BREAK') {
   				return;
   			}
-  			
+
   			// Save result
   			var answer = {};
   		    answer.examId = vm.exam.examDTO.id;
@@ -329,16 +329,16 @@
   		    }
   		    // answer.audioLink;
   		    // answer.description;
-  		    
+
   			Answer.save(answer, onSaveAnswerSuccess, onSaveAnswerError);
-  			
+
   			function onSaveAnswerSuccess() {
   	  		}
-  	  		
+
   	  		function onSaveAnswerError() {
   	  		}
   		}
-  		
+
   		function saveAnswerSpeaking(selectedQuestionId, audioLink) {
   			var answer = {};
   		    answer.examId = vm.exam.examDTO.id;
@@ -346,12 +346,12 @@
   		    answer.audioLink = audioLink;
   		 	answer.status = 'MARKING';
   		    // answer.description;
-  		    
+
   			Answer.save(answer, onSaveAnswerSuccess, onSaveAnswerError);
-  			
+
   			function onSaveAnswerSuccess() {
   	  		}
-  	  		
+
   	  		function onSaveAnswerError() {
   	  		}
   		}
