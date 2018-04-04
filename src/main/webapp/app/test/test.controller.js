193fa1dda076689312b3b56a4679e1d05fb47680
Phuongna <phuongna@itsol.vn>
2018-04-04 10:31:34 +0700

    fix

--- src/main/webapp/app/test/test.controller.js
+++ src/main/webapp/app/test/test.controller.js
@@ -10,13 +10,13 @@
 
     function TestController ($controller, $scope, $window, $stateParams, Principal, LoginService, $state
     		, $rootScope, $timeout, ExamType, Exam, Answer, Upload, $sce) {
-        
+
     	var vm = this;
     	// Function
     	vm.answer = answer;
     	vm.closeExam = closeExam;
     	vm.trustAsHtml = $sce.trustAsHtml;
-    	
+
     	// Variable, flag
     	vm.examTypeId;
     	vm.exam;
@@ -31,8 +31,8 @@
     	vm.fileUpload;
     	vm.btnEnable = true;
     	vm.toggleRecording = toggleRecording;
-    	
-    	
+
+
     	function toggleRecording() {
     	    if ($('#record').hasClass("recording")) {
     	        // stop recording
@@ -49,56 +49,56 @@
     	        audioRecorder.record();
     	    }
     	}
-    	
+
     	function initPlayer() {
-    		var audio = $("#player");      
+    		var audio = $("#player");
             audio[0].addEventListener('ended', callBackAudioEnded);
     	}
-    	
+
     	function callBackAudioEnded() {
     		console.log('audio ended!');
     	}
-    	
+
     	function playAudio(link, timeout) {
-    		var audio = $("#player");      
+    		var audio = $("#player");
             $("#mp3_src").attr("src", link); // https://storage.googleapis.com/pte-magic/CHINA_1.mp3
             audio[0].pause();
             audio[0].load();
-            
+
             $timeout(function(){
             	audio[0].play();
             }, timeout );
     	}
-    	
+
     	function initAnswer() {
     		// Stop audio
-    		var audio = $("#player");      
+    		var audio = $("#player");
             audio[0].pause();
-            
+
             // Stop timer
             $scope.$broadcast('timer-stop');
   			$scope.$broadcast('timer-reset');
     	}
-    	
+
     	angular.element(document).ready(function () {
     		$timeout(function(){
 	    		// Load player
 	    		initPlayer();
-	    		
+
 	    		// Load record audio
 	    		initAudio();
     		}, 1000 );
         });
-    	
+
     	// Init controller
   		(function initController() {
   			// instantiate base controller
 			//$controller('PteMagicBaseController', {
 			//	vm : vm
 			//});
-  					
+
   			vm.examTypeId = $stateParams.type;
-  			
+
   			Exam.startExams({
   				examTypeId: vm.examTypeId
             }, onSuccess, onError);
@@ -106,7 +106,7 @@
             	vm.exam = data;
             	vm.questions = data.questions;
             	console.log(data);
-            	
+
             	// Next question
             	nextQuestion();
             }
@@ -117,10 +117,10 @@
   		function closeExam() {
   			$window.close();
   		}
-  		
+
   		function getUserAnswer() {
   			vm.answers = [];
-  			
+
   			if (vm.selectedQuestion.type == 'LISTENING_FIB_L') {
   				$('.input_answer').each(function(){
   					vm.answers.push($(this).val());
@@ -134,10 +134,10 @@
   	            });
   			}
   		}
-  		
+
   		function answer() {
   			initAnswer();
-  			
+
   			// Upload if questionGroup == SPEAKING
   			if (vm.questionGroup == 'SPEAKING') {
   				uploadRecording(vm.selectedQuestion.id);
@@ -146,15 +146,15 @@
   	  			// Get answer
   	  			getUserAnswer();
   	  			console.log(vm.answers);
-  	  			
+
   	  			// Save answer
   	  			saveAnswer();
   			}
-  			
+
   			// Next question
   			nextQuestion();
   		}
-  		
+
   		$scope.$watch('vm.fileUpload', function (file) {
   			var file;
   			if (file) {
@@ -172,12 +172,12 @@
                   if (response.status > 0)
                       $scope.errorMsg = response.status + ': ' + response.data;
               }, function (evt) {
-                  file.progress = Math.min(100, parseInt(100.0 * 
+                  file.progress = Math.min(100, parseInt(100.0 *
                                            evt.loaded / evt.total));
               });
-          }  
+          }
         });
-  		
+
   		function uploadRecording(selectedQuestionId) {
   			var blobUrl = $("#save").attr('href');
   			console.log(blobUrl);
@@ -191,23 +191,23 @@
   			    	// myBlob is now the blob that the object URL pointed to.
   			    	var filename = "recording_" + vm.exam.examDTO.id + "_" + selectedQuestionId + ".wav";
   			    	vm.fileUpload = new File([blob], filename);
-  			    	
+
   			    	// save answer
   			    	saveAnswerSpeaking(selectedQuestionId, filename);
   			  }
   			};
   			xhr.send();
   		}
-  		
+
   		function updateQuestionInfo(selQuestion) {
   			// Replace @Blank@
   			if (selQuestion.type == 'LISTENING_FIB_L') {
-  				selQuestion.description = selQuestion.description.replace(/@Blank@/g, '<input type="text" name="input" class="input_answer"/>');
+  				selQuestion.description = selQuestion.description.replace(/@Blank@/g, '<input type="text" name="input" class="input_answer pte-writing-input"/>');
   				//selQuestion.description.split('@Blank@').join('xxxxxxx');
   			}
   		}
-  		
-  		
+
+
   		function nextQuestion() {
   			vm.selectedQuestion = vm.questions.shift();
   			if (vm.selectedQuestion == null || vm.selectedQuestion == undefined) {
@@ -229,24 +229,24 @@
   			} else {
   				// Get question group
   				updateQuestionInfo(vm.selectedQuestion);
-  				
+
   				console.log(vm.selectedQuestion);
-  				
+
   				vm.questionGroup = getQuestionGroup(vm.selectedQuestion.type);
   				console.log(vm.questionGroup);
-  				
+
   				// Enable/disable button Answer
   				if (vm.questionGroup == 'SPEAKING') {
   					vm.btnEnable = false;
   				} else {
   					vm.btnEnable = true;
   				}
-  				
+
   				$scope.$broadcast('timer-start');
   				playAudio(vm.selectedQuestion.audioLink, 3000);
   			}
   		}
-  		
+
   		function saveAnswer() {
   			// Save result
   			var answer = {};
@@ -255,16 +255,16 @@
   		    answer.answer = vm.answers.join(',');;
   		    // answer.audioLink;
   		    // answer.description;
-  		    
+
   			Answer.save(answer, onSaveAnswerSuccess, onSaveAnswerError);
-  			
+
   			function onSaveAnswerSuccess() {
   	  		}
-  	  		
+
   	  		function onSaveAnswerError() {
   	  		}
   		}
-  		
+
   		function saveAnswerSpeaking(selectedQuestionId, audioLink) {
   			var answer = {};
   		    answer.examId = vm.exam.examDTO.id;
@@ -272,12 +272,12 @@
   		    answer.audioLink = audioLink;
   		 	answer.status = 'MARKING';
   		    // answer.description;
-  		    
+
   			Answer.save(answer, onSaveAnswerSuccess, onSaveAnswerError);
-  			
+
   			function onSaveAnswerSuccess() {
   	  		}
-  	  		
+
   	  		function onSaveAnswerError() {
   	  		}
   		}
