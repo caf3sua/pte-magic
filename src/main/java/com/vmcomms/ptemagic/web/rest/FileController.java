2f20c2bc61269f09eef53914e681d941b50603ee
Le Viet Dung <fsoft.dunglv@gmail.com>
2018-05-17 02:45:18 +0700

    - Fix download bug when use jar file

--- src/main/java/com/vmcomms/ptemagic/web/rest/FileController.java
+++ src/main/java/com/vmcomms/ptemagic/web/rest/FileController.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.codec.binary.StringUtils;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -44,106 +45,108 @@ import com.vmcomms.ptemagic.web.rest.errors.BadRequestAlertException;
 @RequestMapping("/api/file")
 public class FileController {
 
-	private final Logger log = LoggerFactory.getLogger(FileController.class);
+    private final Logger log = LoggerFactory.getLogger(FileController.class);
 
-	// Save the uploaded file to this folder
-	private static String UPLOADED_FOLDER = "D://etc//";
+    // Save the uploaded file to this folder
+    private static String UPLOADED_FOLDER = "D://etc//";
 
-	@Autowired
-	private MarkScoreService markScoreService;
-	
-	@Autowired
-	private ExamTypeService examTypeService;
+    @Autowired
+    private MarkScoreService markScoreService;
 
-	@PostMapping("/upload/{type}") // //new annotation since 4.3
-	public FileDTO singleFileUpload(@RequestParam("file") MultipartFile file, @PathVariable String type) {
+    @Autowired
+    private ExamTypeService examTypeService;
 
-		FileDTO fileDTO = new FileDTO();
+    @PostMapping("/upload/{type}") // //new annotation since 4.3
+    public FileDTO singleFileUpload(@RequestParam("file") MultipartFile file, @PathVariable String type) {
 
-		if (file.isEmpty()) {
-			throw new BadRequestAlertException("A new question cannot already have an ID", "File", "file-not-found");
-		}
+        FileDTO fileDTO = new FileDTO();
 
-		try {
+        if (file.isEmpty()) {
+            throw new BadRequestAlertException("A new question cannot already have an ID", "File", "file-not-found");
+        }
 
-			String filename = markScoreService.processUploadToCloud(file, type);
+        try {
 
-			fileDTO.setFilename(filename);
+            String filename = markScoreService.processUploadToCloud(file, type);
 
-			return fileDTO;
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+            fileDTO.setFilename(filename);
 
-		return null;
-	}
+            return fileDTO;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-	@PostMapping("/create_mock_test") // //new annotation since 4.3
-	public MockExamDTO createMockTest(@RequestParam("file") MultipartFile file) {
-		MockExamDTO mockExam = new MockExamDTO();
-		
-		if (file.isEmpty()) {
-			throw new BadRequestAlertException("A new question cannot already have an ID", "File", "file-not-found");
-		}
+        return null;
+    }
 
-		try {
+    @PostMapping("/create_mock_test") // //new annotation since 4.3
+    public MockExamDTO createMockTest(@RequestParam("file") MultipartFile file) {
+        MockExamDTO mockExam = new MockExamDTO();
 
-			// Get the file and save it somewhere
+        if (file.isEmpty()) {
+            throw new BadRequestAlertException("A new question cannot already have an ID", "File", "file-not-found");
+        }
+
+        try {
+
+            // Get the file and save it somewhere
 //			byte[] bytes = file.getBytes();
 //			Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
 //			Files.write(path, bytes);
-			File f = convert(file);
-			parseExcelFile(f);
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+            File f = convert(file);
+            parseExcelFile(f);
 
-		return mockExam;
-	}
-	
-	private File convert(MultipartFile file) throws IOException
-	{    
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return mockExam;
+    }
+
+    private File convert(MultipartFile file) throws IOException {
 //	    File convFile = new File(file.getOriginalFilename());
-//	    convFile.createNewFile(); 
-		File convFile = File.createTempFile("tmp", ".xlsx");
-	    FileOutputStream fos = new FileOutputStream(convFile); 
-	    fos.write(file.getBytes());
-	    fos.close();
-	    
-	    return convFile;
-	}
+//	    convFile.createNewFile();
+        File convFile = File.createTempFile("tmp", ".xlsx");
+        FileOutputStream fos = new FileOutputStream(convFile);
+        fos.write(file.getBytes());
+        fos.close();
 
-	@GetMapping("/download_template")
-	public void getExcell(final HttpServletRequest request, final HttpServletResponse response) throws IOException {
-		File file = new ClassPathResource("templates/mock_test_template.xlsx").getFile();
-		
-		try (InputStream fileInputStream = new FileInputStream(file);
-                OutputStream output = response.getOutputStream();) {
+        return convFile;
+    }
+
+    @GetMapping("/download_template")
+    public void getExcell(final HttpServletRequest request, final HttpServletResponse response) throws IOException {
+        InputStream fileInputStream = new ClassPathResource("templates/mock_test_template.xlsx").getInputStream();
+        File fileDownload = File.createTempFile("test", ".xlsx");
+        try (InputStream inputStream = new FileInputStream(fileDownload);
+             OutputStream output = response.getOutputStream()) {
+            FileUtils.copyInputStreamToFile(fileInputStream, fileDownload);
 
             response.reset();
 
             response.setContentType("application/octet-stream");
-            response.setContentLength((int) (file.length()));
+            response.setContentLength((int) fileDownload.length());
 
-            response.setHeader("Content-Disposition", "attachment; filename=\"" + file.getName() + "\"");
+            response.setHeader("Content-Disposition", "attachment; filename=\"" + "mock_test_template.xlsx" + "\"");
 
-            IOUtils.copyLarge(fileInputStream, output);
+            IOUtils.copyLarge(inputStream, output);
             output.flush();
         } catch (IOException e) {
             log.error(e.getMessage(), e);
+        } finally {
+            IOUtils.closeQuietly(fileInputStream);
         }
-	}
+    }
 
-	
-	@SuppressWarnings("deprecation")
-	private MockExamDTO parseExcelFile(File file) {
-		MockExamDTO mockExam = new MockExamDTO();
-		ExamTypeDTO examType = new ExamTypeDTO();
-		mockExam.setExamTypeDTO(examType);
-		
-		Workbook workbook = null;
-		try {
+
+    @SuppressWarnings("deprecation")
+    private MockExamDTO parseExcelFile(File file) {
+        MockExamDTO mockExam = new MockExamDTO();
+        ExamTypeDTO examType = new ExamTypeDTO();
+        mockExam.setExamTypeDTO(examType);
+
+        Workbook workbook = null;
+        try {
             FileInputStream excelFile = new FileInputStream(file);
             workbook = new XSSFWorkbook(excelFile);
             Sheet datatypeSheet = workbook.getSheetAt(0);
@@ -153,73 +156,73 @@ public class FileController {
             while (iterator.hasNext()) {
 
                 Row currentRow = iterator.next();
-                
+
                 // Type
                 if (currentRow.getRowNum() == 0) {
-                	Cell currentCell = currentRow.getCell(1); 
-                	examType.setType(TestType.valueOf(currentCell.getStringCellValue()));
-                	// Name
+                    Cell currentCell = currentRow.getCell(1);
+                    examType.setType(TestType.valueOf(currentCell.getStringCellValue()));
+                    // Name
                 } else if (currentRow.getRowNum() == 1) {
-                	Cell currentCell = currentRow.getCell(1); 
-                	examType.setName(currentCell.getStringCellValue());
-                	// Time
-				} else if (currentRow.getRowNum() == 2) {
-					Cell currentCell = currentRow.getCell(1); 
-                	examType.setTotalTime((long)currentCell.getNumericCellValue() * 60); // convert to seconds
-					// Limit Exam SILVER
-				} else if (currentRow.getRowNum() == 3) {
-					Cell currentCell = currentRow.getCell(1); 
-                	examType.setLimitTestSilver((int)currentCell.getNumericCellValue());
-					// Limit Exam GOLD 
-				} else if (currentRow.getRowNum() == 4) {
-					Cell currentCell = currentRow.getCell(1); 
-                	examType.setLimitTestGold((int)currentCell.getNumericCellValue());
-					// Limit Exam PLATINUM
-				} else if (currentRow.getRowNum() == 5) {
-					Cell currentCell = currentRow.getCell(1); 
-                	examType.setLimitTestPlatinum((int)currentCell.getNumericCellValue());					
-					// Title
-				} else if (currentRow.getRowNum() == 6) {
-					
-					// Question
-				} else {
-					parseQuestion(mockExam, currentRow, orderQuestion);
-					orderQuestion++;
-				}
+                    Cell currentCell = currentRow.getCell(1);
+                    examType.setName(currentCell.getStringCellValue());
+                    // Time
+                } else if (currentRow.getRowNum() == 2) {
+                    Cell currentCell = currentRow.getCell(1);
+                    examType.setTotalTime((long) currentCell.getNumericCellValue() * 60); // convert to seconds
+                    // Limit Exam SILVER
+                } else if (currentRow.getRowNum() == 3) {
+                    Cell currentCell = currentRow.getCell(1);
+                    examType.setLimitTestSilver((int) currentCell.getNumericCellValue());
+                    // Limit Exam GOLD
+                } else if (currentRow.getRowNum() == 4) {
+                    Cell currentCell = currentRow.getCell(1);
+                    examType.setLimitTestGold((int) currentCell.getNumericCellValue());
+                    // Limit Exam PLATINUM
+                } else if (currentRow.getRowNum() == 5) {
+                    Cell currentCell = currentRow.getCell(1);
+                    examType.setLimitTestPlatinum((int) currentCell.getNumericCellValue());
+                    // Title
+                } else if (currentRow.getRowNum() == 6) {
+
+                    // Question
+                } else {
+                    parseQuestion(mockExam, currentRow, orderQuestion);
+                    orderQuestion++;
+                }
             }
-            
+
             workbook.close();
-            
-            // Save 
+
+            // Save
             mockExam = examTypeService.saveMockTest(mockExam);
         } catch (FileNotFoundException e) {
-        	log.error(e.getMessage(), e);
+            log.error(e.getMessage(), e);
         } catch (IOException e) {
-        	log.error(e.getMessage(), e);
+            log.error(e.getMessage(), e);
         }
-		
-		return mockExam;
-	}
-		
-	private void parseQuestion(MockExamDTO mock, Row currentRow, int orderId) {
-		ConfigMockExamDTO configMock = new ConfigMockExamDTO();
-		Cell groupCell = currentRow.getCell(0); 
-		Cell typeCell = currentRow.getCell(1); 
-		Cell questionCell = currentRow.getCell(2);
-		
-		configMock.setOrderId(orderId);
-		configMock.setQuestionGroup(groupCell.getStringCellValue());
-		configMock.setQuestionType(typeCell.getStringCellValue());
-		configMock.setQuestionId((long)questionCell.getNumericCellValue());
-		
-		if (StringUtils.equals("SPEAKING", configMock.getQuestionGroup())) {
-			mock.getLstSpeaking().add(configMock);
-		} else if (StringUtils.equals("WRITING", configMock.getQuestionGroup())) {
-			mock.getLstWriting().add(configMock);
-		} else if (StringUtils.equals("READING", configMock.getQuestionGroup())) {
-			mock.getLstReading().add(configMock);
-		} else if (StringUtils.equals("LISTENING", configMock.getQuestionGroup())) {
-			mock.getLstListening().add(configMock);
-		}
-	}
+
+        return mockExam;
+    }
+
+    private void parseQuestion(MockExamDTO mock, Row currentRow, int orderId) {
+        ConfigMockExamDTO configMock = new ConfigMockExamDTO();
+        Cell groupCell = currentRow.getCell(0);
+        Cell typeCell = currentRow.getCell(1);
+        Cell questionCell = currentRow.getCell(2);
+
+        configMock.setOrderId(orderId);
+        configMock.setQuestionGroup(groupCell.getStringCellValue());
+        configMock.setQuestionType(typeCell.getStringCellValue());
+        configMock.setQuestionId((long) questionCell.getNumericCellValue());
+
+        if (StringUtils.equals("SPEAKING", configMock.getQuestionGroup())) {
+            mock.getLstSpeaking().add(configMock);
+        } else if (StringUtils.equals("WRITING", configMock.getQuestionGroup())) {
+            mock.getLstWriting().add(configMock);
+        } else if (StringUtils.equals("READING", configMock.getQuestionGroup())) {
+            mock.getLstReading().add(configMock);
+        } else if (StringUtils.equals("LISTENING", configMock.getQuestionGroup())) {
+            mock.getLstListening().add(configMock);
+        }
+    }
 }
